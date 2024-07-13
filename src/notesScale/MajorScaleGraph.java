2e9d5e3457153f4d2a8226dd19a12613fd5128ba
vieirandre <andre.vieira@outlook.com>
2024-07-13 16:45:48 -0300

    Apply improvements for MajorScaleGraph

--- src/notesScale/MajorScaleGraph.java
+++ src/notesScale/MajorScaleGraph.java
@@ -1,52 +1,44 @@
 package notesScale;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class MajorScaleGraph {
 
 	private static final int[] MAJOR_SCALE_PATTERN = {2, 2, 1, 2, 2, 2, 1}; // iniciando um array com os passos de tom e semitom
-	private String[] notes = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" }; // inciando um array com as notas
+	private final Map<Note, Set<Note>> nodes = new HashMap<>();
 
-	private Map<String, List<String>> nodes = new HashMap<String, List<String>>(); // iniciando um hashmap para representar as vertices e com quais estao conectadas
-
-	private void addNode(String node) {
-		nodes.put(node, new ArrayList<String>()); // adicionando uma vertice no grafo
+	private void addNode(Note note) {
+		nodes.put(note, new LinkedHashSet<>()); // adicionando uma vertice no grafo
 	}
 
-	private void addEdge(String node, String value) {
-		List<String> edges = nodes.get(node);
+	private void addEdge(Note note, Note value) {
+		Set<Note> edges = nodes.get(note);
 		edges.add(value); // adicinando uma aresta em um vertice, para conecta-lo em outro vertice.
-
-		nodes.put(node, edges); // atualizando as arestas do vertice
+		nodes.put(note, edges);
 	}
 
 	public void init() {
-		// percorre a lista de notas de gera a escala maior de cada uma
+		Note[] notes = Note.values();
+		// percorre a lista de notas e gera a escala maior de cada uma
 		for (int i = 0; i < notes.length; i++) {
-			String note = notes[i];
-
+			Note note = notes[i];
 			addNode(note);
-
-			int nextNoteFromScaleIndex = i;
+			int nextNoteIndex = i;
 
 			// executa o padrao e conecta nota encontrada na vertice da nota tonica da escala
-			for (int tone : MAJOR_SCALE_PATTERN) {
-				nextNoteFromScaleIndex = (nextNoteFromScaleIndex + tone) % 12;
-
-				String nextNoteFromScale = notes[nextNoteFromScaleIndex];
-
-				addEdge(note, nextNoteFromScale);
+			for (int step : MAJOR_SCALE_PATTERN) {
+				nextNoteIndex = (nextNoteIndex + step) % notes.length;
+				Note nextNote = notes[nextNoteIndex];
+				addEdge(note, nextNote);
 			}
 		}
 	}
 
 	// retorna a escala de acordo com a n
-	public List<String> getMajorScaleFromNote(String note) {
-		List<String> majorScale = nodes.get(note);
-
-		return majorScale;
+	public Set<Note> getMajorScaleFromNote(Note note) {
+		return nodes.getOrDefault(note, new LinkedHashSet<>());
 	}
 }
